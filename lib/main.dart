ff9ef801cefb87814103c50a818b57f4449502bc
Abhishek-165 <shettyakshay041@gmail.com>
2020-03-18 12:27:14 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Scaffold(
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
